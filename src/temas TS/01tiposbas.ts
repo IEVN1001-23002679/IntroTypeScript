f9d83f9c920de21fa5924d6f8307eb9e292f72f8
IEVN1001-23002679 <cit.gonzalez004@gmail.com>
2026-09-05 09:59:30 -0600

    Operaciones basicas 05/09/26

---
+++ src/temas TS/01tiposbas.ts
@@ -0,0 +1,12 @@
+console.log("Hola mundo")
+
+var num = 23
+let num1 = 22
+const num2 = 21
+
+let nombres : string
+let numeros : number
+let boleano : boolean
+
+let suma : number = num+num1+num2
+console.log("La suma es:",suma)
